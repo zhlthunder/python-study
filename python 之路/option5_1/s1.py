0eedec55175c173484f778d2a6ee70565de69472
zhlthunder <zhlthunder@163.com>
2017-07-02 18:21:39 +0800

    change to python之路更新38

---
+++ python 之路/option5_1/s1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#author:zhl
+
+##设计模式之工厂模式
+# class msg(object):
+#
+#     def mail(self,msg):
+#         pass
+#
+#     def sms(self,msg):
+#         pass
+#
+#     def weixin(self,msg):
+#         pass
+    # 。。。。
+#当类似的方法有很多时，可能用户就不知道如何去调用，就建议采用工厂模式：
+#工厂模式，即将一些功能相同的方法，封装起来提供一个统一的接口
+# class msg(object):
+#     def sender(self,msg,msg_type):
+#         if msg_type=='sms':
+#             self.sms(msg)
+#         elif msg_type=='mail':
+#             self.mail(msg)
+#
+
+##单例模式：
+
+
+
