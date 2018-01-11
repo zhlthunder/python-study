15d44dbda993f37f942480960e1445c3cbaa826d
zhlthunder <zhlthunder@163.com>
2018-01-11 22:02:05 +0800

    change to python之路更新120

--- python爬虫之路/网页爬虫实战/IP代理构建_9.py
+++ python爬虫之路/网页爬虫实战/IP代理构建_9.py
@@ -7,12 +7,34 @@
 # http://www.xicidaili.com/
 
 #通过API 获取代理IP
+# import urllib.request
+# import re
+# url="http://www.66ip.cn/nmtq.php?getnum=1&isp=0&anonymoustype=0&start=&ports=&export=&ipaddress=&area=2&proxytype=2&api=66ip"
+# data=urllib.request.urlopen(url).read().decode("GBK","ignore")
+# print(len(data))
+# urllib.request.urlretrieve(url,"test.html")
+# pat="(\d{2,3}\..*?)<br"
+# ret=re.compile(pat).findall(data)
+# print(ret)
+
+
+#IP代理的构建：
+#目的： 在对方看来，你就不是使用自己的IP去访问对方，而是使用代理IP访问对方，是一种避免被屏蔽的有效的方法
 import urllib.request
-import re
-url="http://www.66ip.cn/nmtq.php?getnum=1&isp=0&anonymoustype=0&start=&ports=&export=&ipaddress=&area=2&proxytype=2&api=66ip"
-data=urllib.request.urlopen(url).read().decode("GBK","ignore")
+# ip='122.114.31.177:808' #代理IP
+# ##代理IP配置，并安装为全局
+# #————————————————————————
+# proxy=urllib.request.ProxyHandler({"http":ip})  ##将代理IP封装成一个对象的格式
+# # print(proxy)
+# opener=urllib.request.build_opener(proxy,urllib.request.HTTPHandler)
+# urllib.request.install_opener(opener)
+#————————————————————————
+url="http://www.baidu.com"
+data1=urllib.request.urlopen(url).read()
+data=data1.decode("GBK","ignore")
+# print(data)
 print(len(data))
-urllib.request.urlretrieve(url,"test.html")
-pat="(\d{2,3}\..*?)<br"
-ret=re.compile(pat).findall(data)
-print(ret)
+fh=open("test.html","w")
+fh.write(data)
+fh.close()
+
